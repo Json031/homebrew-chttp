b7165ddd58a35c8a4bf4fe5058f18778104dfe73
MorganChen <cjfmail@foxmail.com>
2025-11-09 15:31:03 +0800

    Update desc

--- Formula/chttp.rb
+++ Formula/chttp.rb
@@ -1,5 +1,5 @@
 class Chttp < Formula
-  desc "A tiny C HTTP library using libcurl and cJSON"
+  desc "A open-source C library for http request"
   homepage "https://github.com/json031/chttp"
   url "https://github.com/json031/chttp/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "6fd22b6cfad7078180b3fd76f4a2c1970672015786701c90595d91037320e1ba"
